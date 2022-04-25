6bd1573a7aaabfc7c8e4cdfaae174417ff5fc61c
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-04-25 19:45:15 +0530

    Create Output.py

---
+++ Assignment 1/Output.py
@@ -0,0 +1,33 @@
+#Question 1
+Enter the numbers in the format(x y z):10 20 30
+Average of the three numbers is=  20.0
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 2
+Enter the Gross Income :50000
+Enter the number of Dependents :2
+Tax to be paid =  6800.0
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 3
+Enter SID:21102035
+Enter Name:Vasu
+Enter Gender (F: female)(M: male)(U: unknown):M
+Enter the Course Name:Introduction to Computing
+Enter the CGPA(to 1 decimal place):9.2
+list['21102035', 'Vasu', 'M', 'Introduction to Computing', '9.2']
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 4
+Enter the marks of 5 students separated by a space :40 30 25 60 44
+[25, 30, 40, 44, 60]
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 5
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
