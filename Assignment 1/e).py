bc29e68b602b12ee3b294594b5a4363020715238
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-04-25 19:34:54 +0530

    Delete e).py

--- Assignment 1/e).py
+++
@@ -1,8 +0,0 @@
-COLOUR_LIST=["Red","Green","White","Black","Pink","Yellow"]    #original list
-print(COLOUR_LIST)
-
-del COLOUR_LIST[3]
-print(COLOUR_LIST)      #list obtained after deleting colour black
-
-COLOUR_LIST[3]="Purple"
-print(COLOUR_LIST)       #list obtained after replacing 4th and 5th element with pink colour
