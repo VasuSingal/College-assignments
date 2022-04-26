a9cc11903381852aa08736ab2d930419b76762f0
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-04-26 13:40:12 +0530

    Update Codes.py

--- Assignment 1/Codes.py
+++ Assignment 1/Codes.py
@@ -24,7 +24,7 @@ SID = input("Enter SID:")
 name = input("Enter Name:")
 gender = input("Enter Gender (F: female)(M: male)(U: unknown):")
 course_name = input("Enter the Course Name:")
-CGPA = input("Enter the CGPA(to 1 decimal place):")
+CGPA = float(input("Enter the CGPA(to 1 decimal place):"))
 
 Student = list[SID, name, gender, course_name, CGPA]
 
