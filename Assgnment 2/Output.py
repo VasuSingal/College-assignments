eabac1f3abdc090e521e4d8d5c2dc20456301a32
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-05-08 15:34:42 +0530

    Create Output.py

---
+++ Assgnment 2/Output.py
@@ -0,0 +1,46 @@
+# Question 1
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Question 2
+Hey, Vasu Singal Here!
+My SID is 21102035 
+I am from Civil Engineering department and my CGPA is 8.56
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Question 3
+8
+58
+50
+224
+40
+14
+0
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Question 4
+Enter the string:My name is Vasu
+Yes
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Question 5
+Enter the length of side 1:3
+Enter the length of side 2:4
+Enter the length of side 3:5
+Yes
+
+#-------------------------------------------------------------------------------------------------------------
+
+# Question 6
+Enter the number = 54
+Enter the number = 92
+4
