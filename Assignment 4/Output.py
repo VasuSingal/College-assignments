0a104dc4c41368dd5d8f1e1b7796d3edfa62a681
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-06-05 22:29:12 +0530

    Create Output.py

---
+++ Assignment 4/Output.py
@@ -0,0 +1,48 @@
+#Question 1
+Enter marks = 80
+Grade : A
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 2
+Enter the year = 2003
+2003 is not a Leap year
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 3
+Question  1 : 1 x 4 =
+4
+Right!
+Question  2 : 1 x 2 =
+2
+Right!
+Question  3 : 0 x 2 =
+0
+Right!
+Question  4 : 5 x 7 =
+20
+Wrong. The answer is 35
+Question  5 : 3 x 6 =
+18
+Right!
+Question  6 : 5 x 6 =
+32
+Wrong. The answer is 30
+Question  7 : 1 x 0 =
+0
+Right!
+Question  8 : 0 x 9 =
+0
+Right!
+Question  9 : 0 x 9 =
+0
+Right!
+Question  10 : 1 x 8 =
+7
+Wrong. The answer is 8
+
+#-------------------------------------------------------------------------------------------------
+
+#Question 4
+177 candies are in the bowl!
