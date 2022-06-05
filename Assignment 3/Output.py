5182c7a07eb7952e2dd2a4bc20ecd7838c57ce12
VasuSingal <97177511+VasuSingal@users.noreply.github.com>
2022-06-05 22:08:32 +0530

    Create Output.py

---
+++ Assignment 3/Output.py
@@ -0,0 +1,67 @@
+#Question 1
+Enter the number=69
+000101
+
+
+
+#Question 2
+Operator: *
+a: 32
+b: 65
+2080.0
+
+
+
+#Question 3
+35
+Enter the value of n= 3
+3.0
+Enter the value of r= 9
+1017.8760197630929
+Enter the value of a = 22
+Enter the value of b = 22
+Enter the value of r = 3
+1.7320508075688774
+Enter the value of y1 = 10
+Enter the value of y2 = 20
+Enter the value of x1 = 30
+Enter the value of x2 = 20
+-1.0
+
+
+
+#Question 4
+0
+1
+2
+3
+4
+
+
+3
+4
+5
+6
+7
+8
+9
+
+
+4
+7
+10
+
+
+15
+13
+11
+9
+7
+
+
+
+#Question 5
+Enter the number of Hydrogen atoms = 3
+Enter the number of Carbon atoms = 4
+Enter the number of Oxygen atoms = 5
+Molecular Weight of the compound =  131.06362000000001
